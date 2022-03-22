5456eb76b75d1126a66516ac8d6413fe566a59ee
Francisco <francisco.herrera@open.deusto.es>
2022-03-22 11:23:51 +0100

    Reformatted Packages & Made Application Working

---
+++ src/main/java/ControllerPackage/MainController.java
@@ -0,0 +1,15 @@
+package ControllerPackage;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+
+    @FXML
+    private Label instructionsText;
+
+    @FXML
+    protected void onLogInButtonClick() {
+        instructionsText.setText("Log In is not set-up yet");
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,10 @@ module com.example.recipeapp {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires kotlin.stdlib;
 
     opens ApplicationPackage to javafx.fxml;
     exports ApplicationPackage;
+    opens ControllerPackage to javafx.fxml;
+    exports ControllerPackage;
 }
