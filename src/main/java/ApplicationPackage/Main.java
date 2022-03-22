7904adccf4861b520ee4ff49e236783d5df43a5c
Francisco <francisco.herrera@open.deusto.es>
2022-03-22 11:23:51 +0100

    Reformatted Packages & Made Application Working

--- src/main/java/ApplicationPackage/Main.java
+++ src/main/java/ApplicationPackage/Main.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class MainApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Welcome to the Recipe App!");
         stage.setScene(scene);
         stage.show();
     }
