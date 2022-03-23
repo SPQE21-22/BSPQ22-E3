371d9bac54c58a86966f18d689fd044735960147
Francisco <francisco.herrera@open.deusto.es>
2022-03-23 10:51:07 +0100

    Reformatted Packages & Made Application Working

--- src/main/java/ControllerPackage/MainController.java
+++ src/main/java/ControllerPackage/MainController.java
@@ -3,6 +3,7 @@ package ControllerPackage;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+
 public class MainController {
 
     @FXML
@@ -12,4 +13,19 @@ public class MainController {
     protected void onLogInButtonClick() {
         instructionsText.setText("Log In is not set-up yet");
     }
+
+
+    ConnectionString connectionString = new ConnectionString("mongodb+srv://group3:<group3>@cluster0.zr68o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+    MongoClientSettings settings = MongoClientSettings.builder()
+            .applyConnectionString(connectionString)
+            .serverApi(ServerApi.builder()
+                    .version(ServerApiVersion.V1)
+                    .build())
+            .build();
+    MongoClient mongoClient = MongoClients.create(settings);
+    MongoDatabase database = mongoClient.getDatabase("test");
+
+
+
+
 }
