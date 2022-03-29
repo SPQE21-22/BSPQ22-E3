4c3e607d9ba24cba0bb4fccfa17b5f0420d7e28b
Xabiergarai <72606708+Xabiergarai@users.noreply.github.com>
2022-03-29 10:55:00 +0200

    Register interface dimensions

--- src/main/java/ApplicationPackage/Main.java
+++ src/main/java/ApplicationPackage/Main.java
@@ -3,6 +3,7 @@ package ApplicationPackage;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,10 +12,14 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("register.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Welcome to the Recipe App!");
+
+        int screenWidth = (int) Screen.getPrimary().getBounds().getWidth();
+        int screenHeight = (int) Screen.getPrimary().getBounds().getHeight();
+        Scene scene = new Scene(fxmlLoader.load(), screenWidth-750,screenHeight-430);
+        stage.setTitle("Welcome to the DeustoFood Recipe App!");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
