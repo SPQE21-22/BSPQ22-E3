3fae3e821e708b547d2c1fc90e8b03ab7b775d6f
Francisco <francisco.herrera@open.deusto.es>
2022-04-02 17:56:20 +0200

    Added cSystem.java interface & MyConnector.java class, updated main

--- src/main/java/ApplicationPackage/Main.java
+++ src/main/java/ApplicationPackage/Main.java
@@ -1,24 +1,18 @@
 package ApplicationPackage;
 
 import MongoConnector.MyConnector;
-import com.mongodb.BasicDBObject;
-import com.mongodb.Block;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.*;
-import com.mongodb.ConnectionString;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.bson.Document;
-//import org.bson.Document;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
+
+
+
 
 public class Main extends Application {
+    @SuppressWarnings("ClassEscapesDefinedScope")
+    public static MyConnector connector;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
@@ -26,12 +20,10 @@ public class Main extends Application {
         stage.setTitle("Welcome to the Recipe App!");
         stage.setScene(scene);
         stage.show();
-
-
     }
 
     public static void main(String[] args) {
-        MyConnector connector =  new MyConnector();
+        connector =  new MyConnector();
         launch();
     }
 }
