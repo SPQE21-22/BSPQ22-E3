1d0f7e187c50502c1d14f12b006c9fd446c0c9aa
Francisco <francisco.herrera@open.deusto.es>
2022-04-02 15:42:03 +0200

    Added Logger & Fixed MongoDB Connection

--- src/main/java/ApplicationPackage/Main.java
+++ src/main/java/ApplicationPackage/Main.java
@@ -2,6 +2,7 @@ package ApplicationPackage;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.Block;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.*;
 import com.mongodb.ConnectionString;
 import javafx.application.Application;
@@ -12,6 +13,8 @@ import org.bson.Document;
 //import org.bson.Document;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 public class Main extends Application {
@@ -24,17 +27,38 @@ public class Main extends Application {
         stage.show();
 
 
-        MongoClient client = MongoClients.create("mongodb+srv://group3:group3@cluster0.zr68o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-        MongoDatabase db = client.getDatabase("appDB");
-        MongoCollection col = db.getCollection("Collection1");
-        System.out.println(col.getNamespace());
-
-        BasicDBObject regexQuery = new BasicDBObject();
-
-        FindIterable<Document> cursor = col.find(regexQuery);
+//        MongoClient client = MongoClients.create();
+//        MongoDatabase db = client.getDatabase("appDB");
+//        MongoCollection col = db.getCollection("Collection1");
+//        System.out.println(col.getNamespace());
+//
+//        BasicDBObject regexQuery = new BasicDBObject();
+//
+//        FindIterable<Document> cursor = col.find(regexQuery);
         //MongoCursor<Document> iterator = cursor.iterator();   // THIS IS WHAT IS NOT WORKING. THE ITERATOR
-        cursor.forEach((Consumer<? super Document>) System.out::println);
+        //cursor.forEach((Consumer<? super Document>) System.out::println);
         //System.out.println(cursor);
+
+//        String connectionString = "mongodb+srv://group3:group3@cluster0.zr68o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; //System.getProperty("mongodb.uri");
+//        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+//            List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
+//            databases.forEach(db -> System.out.println(db.toJson()));
+//        }
+
+        ConnectionString connectionString = new ConnectionString("mongodb+srv://group3:group3@cluster0.zr68o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+        MongoDatabase database = mongoClient.getDatabase("appDB");
+        System.out.println(database.getCollection("Collection1").find());
+        System.out.println("Printed_-----------------------------------------------------");
+        try (MongoCursor<Document> cursor = database.getCollection("Collection1").find().iterator()) {
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+            }
+        }
+
 
 
     }
