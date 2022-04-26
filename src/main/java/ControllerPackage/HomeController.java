68697f0b8827a25e9d2e18af5dbe070f02b16712
rosaliaperezabalde <Olatzbilbao0>
2022-04-26 11:33:42 +0200

    Update 26/04

--- src/main/java/ControllerPackage/HomeController.java
+++ src/main/java/ControllerPackage/HomeController.java
@@ -1,4 +1,20 @@
 package ControllerPackage;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
 public class HomeController {
+    public void onProfileButtonClick(ActionEvent event) throws Exception {
+    try {
+        FXMLLoader fmxloader = new FXMLLoader(getClass().getResource("/ApplicationPackage/Profile.fxml"));
+        Parent root1 = (Parent) fmxloader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root1));
+        stage.show();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
 }
