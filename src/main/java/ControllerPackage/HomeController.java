6fe04a9cfe37a6532cb247e2e11b41ac086d6698
rosaliaperezabalde <Olatzbilbao0>
2022-04-27 11:25:48 +0200

    Changes alerts.

--- src/main/java/ControllerPackage/HomeController.java
+++ src/main/java/ControllerPackage/HomeController.java
@@ -9,6 +9,8 @@ import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -19,6 +21,7 @@ import ControllerPackage.ProfileController;
 import ModelPackage.User;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import static ControllerPackage.LoginController.local_user;
 
@@ -34,12 +37,30 @@ public class HomeController {
             Parent root1 = (Parent) fmxloader.load();
             Stage stage = new Stage();
             stage.setScene(new Scene(root1));
+            stage.setTitle("Your Profile");
             stage.show();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+    }
+    public void onHeartButtonClick(ActionEvent event) throws Exception {
+        try {
+
+            Alert alerta2 = new Alert(Alert.AlertType.INFORMATION);
+            alerta2.setTitle("Sorry, "+local_user.getFirst_name());
+            alerta2.setHeaderText("Window under construction.");
+            Optional<ButtonType> resultado = alerta2.showAndWait();
+
+            if (resultado.get() == ButtonType.OK) {
+                alerta2.close();
+            } else {
+                event.consume();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
