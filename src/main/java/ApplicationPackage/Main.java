3c1011b643a55ae5fcb422384195168d12abdfa8
Francisco <francisco.herrera@open.deusto.es>
2022-05-01 17:54:52 +0200

    Increased Performance in Main.java

--- src/main/java/ApplicationPackage/Main.java
+++ src/main/java/ApplicationPackage/Main.java
@@ -14,8 +14,6 @@ public class Main extends Application {
     @SuppressWarnings("ClassEscapesDefinedScope")
     public static MyConnector connector;
     // Switch Scenes Variables:
-    private Stage window;
-    private AnchorPane anchorPane;
     FXMLLoader loader = new FXMLLoader();
     public static Stage window2;
     public static String message = "";
@@ -24,22 +22,24 @@ public class Main extends Application {
 
     @Override
     public void start(Stage window1) throws IOException {
+
         window2 = window1;
-        this.window = window1;
-        this.window.setTitle("Welcome");
-        this.window.setResizable(false);
+        window2.setTitle("Welcome");
+        window2.setResizable(false);
         this.loader.setLocation(Main.class.getResource("Login.fxml"));
-        this.anchorPane = (AnchorPane)this.loader.load();
-        Scene scene1 = new Scene(this.anchorPane);
-        this.window.setScene(scene1);
-        this.window.show();
+        AnchorPane anchorPane = this.loader.load();
+        Scene scene1 = new Scene(anchorPane);
+        window2.setScene(scene1);
+        window2.show();
         logger.info("This is the Main Logger");
 
     }
 
 
     public static void main(String[] args) {
-        connector =  new MyConnector();
+        if (connector == null) {
+            connector = new MyConnector();
+        }
         launch();
     }
 }
