3c088d6ccc86400a53f0be9d636302b9d79cb821
rosaliaperezabalde <Olatzbilbao0>
2022-05-02 19:46:36 +0200

    Cambios contiperf tests.

--- src/test/ModelPackage/UserTest.java
+++ src/test/ModelPackage/UserTest.java
@@ -1,12 +1,23 @@
 package ModelPackage;
 
 import org.junit.jupiter.api.Test;
-
+import org.junit.Rule;
+import org.databene.contiperf.Required;
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.databene.contiperf.report.EmptyReportModule;
 import static org.junit.jupiter.api.Assertions.*;
 
+@PerfTest(invocations = 5)
+@Required(max = 1200, average = 250)
 class UserTest {
+    //This activates contiperf
+    @Rule
+    public ContiPerfRule i = new ContiPerfRule();
 
+    //Executes the test 300 times, regardless of the number of threads.
     @Test
+    @PerfTest(invocations = 300)
     void userVerification() {
         User testUser = new User("Francisco","123456789");
         assertTrue(testUser.getValid());
--- src/test/MongoConnector/MyConnectorTest.java
+++ src/test/MongoConnector/MyConnectorTest.java
@@ -2,11 +2,24 @@ package MongoConnector;
 
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.Test;
+import org.junit.Rule;
+import org.databene.contiperf.Required;
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.junit.ContiPerfRule;
+import org.databene.contiperf.report.EmptyReportModule;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+@PerfTest(invocations = 5)
+@Required(max = 1200, average = 250)
 class MyConnectorTest {
-
+    //This activates contiperf
+    @Rule
+    public ContiPerfRule i = new ContiPerfRule();
+    // Executes the test repeatedly for at least 10,000 milliseconds (10 seconds)
     @Test
+    @PerfTest(duration = 10000)
     void testMyConnector(){
         MyConnector m = new MyConnector();
         String connectedString = "Connected to -> appDB";
