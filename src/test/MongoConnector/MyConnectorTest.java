a1250ea89c8ab85a7be0cdf245e4846725d92ee6
Francisco <francisco.herrera@open.deusto.es>
2022-05-22 18:28:10 -0500

    Fixed JUnit Tests & added Contiperf Functionality

--- src/test/MongoConnector/MyConnectorTest.java
+++ src/test/MongoConnector/MyConnectorTest.java
@@ -4,6 +4,7 @@ package MongoConnector;
 //import org.junit.Rule;
 import org.databene.contiperf.Required;
 import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.junit.ContiPerfRule;
 import org.junit.*;
 import static org.junit.Assert.*;
 
@@ -11,8 +12,8 @@ import static org.junit.Assert.*;
 @Required(max = 1200, average = 250)
 class MyConnectorTest {
     //This activates contiperf
-   // @Rule
-    //public ContiPerfRule i = new ContiPerfRule();
+    @Rule
+    public ContiPerfRule i = new ContiPerfRule();
     // Executes the test repeatedly for at least 10,000 milliseconds (10 seconds)
     @Test
     @PerfTest(duration = 10000)
